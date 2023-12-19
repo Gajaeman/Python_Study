2727ff497b542a93b78e6fa741a461ee8440e7b1
Gajaeman <153297310+Gajaeman@users.noreply.github.com>
2023-12-20 00:52:10 +0900

    231220_0051
    Programers_starter_01

---
+++ Pro_01.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# url : https://school.programmers.co.kr/learn/courses/30/lessons/120810
+
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+print(solution(3,2))
